40e6e21bb4c40b1cd5521105f77f64430f72eb2d
AaGath <89765070+AaGath@users.noreply.github.com>
2021-11-25 20:22:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,9 +27,9 @@ async def start(client, message):
 
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, message.chat.link, total, "Unknown"))       
-            await db.add_chat(message.chat.id, message.chat.title, message.chat.link)
-        return await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title else message.chat.link), reply_markup=reply_markup)
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            await db.add_chat(message.chat.id, message.chat.title)
+        return await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title), reply_markup=reply_markup)
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
