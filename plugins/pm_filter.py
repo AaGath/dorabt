f8de8a9f42498e64a02080980d43ac0928091a9d
AaGath <89765070+AaGath@users.noreply.github.com>
2021-11-26 13:38:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -72,7 +72,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(f"Hey {query.from_user.first_name} This Not\n For You Request Your Own Movie/Series", show_alert=True)
+        return await query.answer(f"Hey {query.from_user.first_name} This Not For You Request Your Own Movie/Series", show_alert=True)
     try:
         offset = int(offset)
     except:
